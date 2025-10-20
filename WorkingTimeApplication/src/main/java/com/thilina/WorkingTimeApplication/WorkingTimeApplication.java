c40b29fa3c750f9952fbb6822c25ea18e9618e0e
Sachintha1994 <sachinthathilina@gmail.com>
2025-10-20 11:36:32 +0530

    Initial commit

---
+++ WorkingTimeApplication/src/main/java/com/thilina/WorkingTimeApplication/WorkingTimeApplication.java
@@ -0,0 +1,13 @@
+package com.thilina.WorkingTimeApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkingTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkingTimeApplication.class, args);
+	}
+
+}
